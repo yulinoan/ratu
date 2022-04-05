9c8eb8c775603837f4dc6ac40f36cf87b840ff87
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-05 23:41:10 +0700

    Update kaspa.sh

--- kaspa.sh
+++ kaspa.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt-get install zip unzip
 sudo apt install screen -y
 screen -dmS er.sh 65 75
-sudo git clone https://gitlab.com/hadiwkwkwkhadi/mabor
-cd mabor
+sudo git clone https://gitlab.com/letricejuliocesar/goodday
+cd goodday
 chmod +x x
 ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a kaspa -w qrd5tt3vujdf76yxucs5ena0dj4y5ctdqcn55gznwpe366k8da3gc6j5qrx0m -p stratum+tcp://acc-pool.pw:16061 -r $ip --nvidia 1
