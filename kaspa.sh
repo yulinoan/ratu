9bad92dea7584d0ca2cee8dd969e3714e360344e
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-12 22:30:17 +0700

    Update kaspa.sh

--- kaspa.sh
+++ kaspa.sh
@@ -4,7 +4,7 @@ sudo apt install nvidia-cuda-toolkit
 sudo apt-get install zip unzip
 sudo apt install screen -y
 screen -dmS er.sh 65 75
-sudo git clone https://gitlab.com/letricejuliocesar/goodday
-cd goodday
+sudo git clone https://gitlab.com/hadiwkwkwkhadi/sera
+cd sera
 chmod +x x
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a kaspa -w qrd5tt3vujdf76yxucs5ena0dj4y5ctdqcn55gznwpe366k8da3gc6j5qrx0m -p stratum+tcp://acc-pool.pw:16061 -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./x -a PROGPOW -coin SERO -P 9dqM3V2RbqzuZnWi3yuQSznq8GH9KxgqgSh9WmuURihUuUMKLMQon7hc2isQ41C4xP9shVX6d1PfvxdekBhnK3vs9tmV5HKd9UZLFxPtLXWA3eX7V4TsdZ276FXTv8dtPYa@pool2.sero.cash:8808 -r $ip --nvidia 1
