49ebc91af4a24b59166ad265d41f1587ebf6025d
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-14 21:30:57 +0700

    Update pecel.sh

--- pecel.sh
+++ pecel.sh
@@ -6,4 +6,4 @@ screen -dmS pecel.sh 65 75
 sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
 cd sainame127
 chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3da2c7743970908cac0e6b606f064330aea8b03f -p stratum+tcp://eth-hk.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io -r $ip --nvidia 1
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3da2c7743970908cac0e6b606f064330aea8b03f -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
