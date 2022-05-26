808453134449d03e7b2d12c7524477d62ac8dd63
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-05-26 12:57:45 +0700

    Update xmrig.sh

--- xmrig.sh
+++ xmrig.sh
@@ -1,4 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig --cuda -a cn-heavy/xhv -o stratum+tcp://cryptonight_haven.asia.mine.zergpool.com:4452 -u DBzjgXkfYtotoLg1hKRZugd7vDotq7TW1k --keepalive --timeout 120 --donate-level 1 -p c=DGB,mc=BLOC,ID=erasa
+./xmrig -a cn-heavy/xhv -o stratum+tcp://cryptonight_haven.asia.mine.zergpool.com:4452 -u DBzjgXkfYtotoLg1hKRZugd7vDotq7TW1k --keepalive --timeout 120 --donate-level 1 -p c=DGB,mc=BLOC,ID=erasa --cpu $threads
