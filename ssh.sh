b14c58adacc184d696ed01d3cb27441d4d6c0b3e
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-05-29 12:35:01 +0700

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -3,3 +3,4 @@ tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with SSH address: #{tmate_ssh}"
+./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "Connect with web: #{tmate_web}"
